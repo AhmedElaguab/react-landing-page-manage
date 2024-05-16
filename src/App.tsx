b076504b2399f078fecb7aee3084ca4f2bc42024
Ahmed Elaguab <elaguabahmed97@gmail.com>
2024-05-16 02:16:13 +0100

    add intro structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Logo from './assets/images/logo.svg';
 import IconHamburger from './assets/images/icon-hamburger.svg';
 import IconClose from './assets/images/icon-close.svg';
+import IntroIllustration from './assets/images/illustration-intro.svg';
 
 function App() {
   return (
@@ -14,6 +15,16 @@ function App() {
           </button>
         </nav>
       </header>
+      <main className="px-4">
+        <section>
+          <img src={IntroIllustration} alt="intro illustration" />
+          <h1>Bring everyone together to build better products.</h1>
+          <p>
+            Manage makes it simple for software teams to plan day-to-day tasks
+            while keeping the larger team goals in view!
+          </p>
+        </section>
+      </main>
     </div>
   );
 }
