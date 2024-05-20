2ab2ec67aae9076512769d67cc0cfdbc5613a5db
Ahmed Elaguab <elaguabahmed97@gmail.com>
2024-05-20 16:17:04 +0100

    add intro section screen styles

--- src/App.tsx
+++ src/App.tsx
@@ -96,25 +96,29 @@ function App() {
         </nav>
       </header>
       <main className="px-4">
-        <section className="text-center">
-          <img
-            className="mx-auto"
-            src={IntroIllustration}
-            alt="intro illustration"
-          />
-          <h1 className="text-blue-primary text-5xl leading-tight font-bold mt-4">
-            Bring everyone together to build better products.
-          </h1>
-          <p className="text-gray-light text-xl mt-4">
-            Manage makes it simple for software teams to plan day-to-day tasks
-            while keeping the larger team goals in view!
-          </p>
-          <a
-            className="inline-flex bg-red-primary text-white py-3 px-8 mt-8 rounded-full shadow-md shadow-red-primary hover:opacity-90"
-            href="/"
-          >
-            Get Started
-          </a>
+        <section className="text-center lg:text-start lg:flex lg:mt-12">
+          <div className="lg:order-2 lg:w-1/2">
+            <img
+              className="mx-auto lg:flex"
+              src={IntroIllustration}
+              alt="intro illustration"
+            />
+          </div>
+          <div className="lg:w-1/2 lg:flex lg:flex-col lg:justify-center lg:items-baseline">
+            <h1 className="text-blue-primary text-5xl lg:text-6xl lg:leading-tight font-bold mt-4">
+              Bring everyone together to build better products.
+            </h1>
+            <p className="text-gray-light text-xl mt-4 lg:w-5/6">
+              Manage makes it simple for software teams to plan day-to-day tasks
+              while keeping the larger team goals in view!
+            </p>
+            <a
+              className="inline-flex bg-red-primary text-white py-3 px-8 mt-8 rounded-full shadow-md shadow-red-primary hover:opacity-90"
+              href="/"
+            >
+              Get Started
+            </a>
+          </div>
         </section>
         <section className="mt-24 leading-relaxed">
           <h2 className="text-center text-blue-primary text-4xl leading-normal font-bold">
