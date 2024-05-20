524ea41c08db7f462e9a1c40894b9df9c377469a
Ahmed Elaguab <elaguabahmed97@gmail.com>
2024-05-20 16:34:12 +0100

    add simplify section screen styles

--- src/App.tsx
+++ src/App.tsx
@@ -199,12 +199,12 @@ function App() {
         </section>
 
         <section className="mt-24 -mx-4 bg-red-primary text-center text-white py-20">
-          <div className="max-w-[1264px] mx-auto">
-            <h2 className="text-4xl font-medium leading-normal w-[70%] mx-auto">
+          <div className="max-w-[1264px] mx-auto lg:flex lg:justify-between lg:items-center ">
+            <h2 className="text-4xl font-medium leading-normal w-[70%] lg:w-auto mx-auto lg:mx-0">
               Simplify how your team works today.
             </h2>
             <a
-              className="inline-flex bg-white text-red-primary py-3 px-8 mt-8 rounded-full shadow-md hover:opacity-90"
+              className="inline-flex bg-white text-red-primary py-3 px-8 mt-8 lg:mt-0 rounded-full shadow-md hover:opacity-90"
               href="/"
             >
               Get Started
